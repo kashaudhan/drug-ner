8701bcbd0a52625199ec11acf42829114375c739
kashaudhan <kashaudhan94@gmail.com>
2021-12-26 21:25:40 +0530

    settings update

--- drug_ner/settings.py
+++ drug_ner/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.233.0.164', 'localhost']
 
 
 # Application definition
