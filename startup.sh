e20ba9622d22947ef5b5ed35d76eccea1b073940
kashaudhan <kashaudhan94@gmail.com>
2021-12-27 03:39:50 +0530

    fix staryup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # start-server.sh
-(cd nutrizionedb; gunicorn drug-ner.wsgi --user www-data --bind 0.0.0.0:8000 --timeout 600  --worker-class=gevent --worker-connections=1000 --workers=33 ) & nginx -g "daemon off;"
+(cd nutrizionedb; gunicorn drug_ner.wsgi --user www-data --bind 0.0.0.0:8000 --timeout 600  --worker-class=gevent --worker-connections=1000 --workers=33 ) & nginx -g "daemon off;"
 Ks
